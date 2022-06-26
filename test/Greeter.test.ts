4c908211a58c22ae6a1a09d1f54470e6fc16ddb7
Howard <howard.xie@coinbase.com>
2022-06-25 21:23:19 -0700

    20220625 testing with typescript and typechain

--- test/Greeter.test.ts
+++ test/Greeter.test.ts
@@ -1,10 +1,14 @@
 import {ethers} from "hardhat"
 import {expect} from "chai"
+import { Greeter, Greeter__factory } from "../typechain"
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const signers = await ethers.getSigners()
+    const firstSigner = signers[0]
+    
+    const greeterFactory = new Greeter__factory().connect(firstSigner)
+    const greeter = await greeterFactory.deploy("Hello, world!");
     await greeter.deployed();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
