8986c941050d7f6175a946c37a545dacf01d2b9d
Howard <howard.xie@coinbase.com>
2022-06-25 21:15:58 -0700

    typechain able to compile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,13 +5,12 @@ import "@nomiclabs/hardhat-ethers"
 import { HardhatUserConfig } from "hardhat/config"
 
 const config: HardhatUserConfig = {
-
- solidity: {
-  version: "0.8.4"
-},
-typechain: {
-  outDir: "typechain"
-}
+    solidity: {
+        version: "0.8.4"
+    },
+    typechain: {
+        outDir: "typechain"
+    }
 }
 
 export default config;
